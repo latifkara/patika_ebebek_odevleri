1f75f18e6d7675a6cef047c8b0bc7f403a412801
latifkara <latifkara93@gmail.com>
2022-11-04 10:36:22 +0300

    add min max culculcate class

---
+++ odev_hafta_3_1/src/com/javaGelisme/Main.java
@@ -0,0 +1,11 @@
+package com.javaGelisme;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MinMax minMax = new MinMax();
+	    minMax.input();
+	    minMax.culculateMinMax();
+	    minMax.printValue();
+    }
+}
