d10d273fbbd07217e1b637324972f4f575a89f66
latifkara <latifkara93@gmail.com>
2022-11-06 14:37:57 +0300

    add Primer Number class

--- odev_hafta_3_4/src/com/javaGelisme/Main.java
+++ odev_hafta_3_4/src/com/javaGelisme/Main.java
@@ -3,6 +3,7 @@ package com.javaGelisme;
 public class Main {
 
     public static void main(String[] args) {
+        // I got numbers between 1 and 100 with two for loops and printed those without divisors to the screen.
         for (int i = 2 ; i <= 100 ; i++){
             boolean control =  false;
             for (int j = 2 ; j < i ; j++){
