e58981b3b2464c4d0979d5bac68c33ce6f2bc745
latifkara <latifkara93@gmail.com>
2022-11-06 14:32:28 +0300

    add Primer Number class

--- odev_hafta_3_3/src/com/javaGelisme/ReverseStarClass.java
+++ odev_hafta_3_3/src/com/javaGelisme/ReverseStarClass.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 
         Scanner input = new Scanner(System.in);
         int number;
+        // number of digits will be taken from user.
         public void input() {
             System.out.print("Basamak Sayısı: ");
             number = input.nextInt();
         }
+        // With the for loop, I first added the spaces and then added 2*index asterisks.
         public void getReverseStar() {
             for (int i = number; i >= 1; i--) {
                 for (int k = -1; k < (number - i); k++) {
