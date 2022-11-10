64fc1770b9b6c1571af12e23e64a5d1adaa5347b
latifkara <latifkara93@gmail.com>
2022-11-10 12:03:33 +0300

    Add Matrix Transpose class

---
+++ odev_hafta_4_4/src/com/javaGelisme/Main.java
@@ -0,0 +1,10 @@
+package com.javaGelisme;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // I defined a new object from the FindMatrixTranspose class and called the print method.
+	    FindMatrixTranspose findMatrix = new FindMatrixTranspose();
+	    findMatrix.print();
+    }
+}
